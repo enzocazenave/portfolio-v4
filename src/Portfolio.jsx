c6fba6ca0444b3cd8b0ebed5f61d025d4149919a
Enzo <encaze24@hotmail.com>
2023-03-16 22:05:20 -0300

    Add changes

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -6,19 +6,6 @@ import {
 } from './styles/modules/Portfolio.module.css'
 
 export const Portfolio = ({  }) => {
-
-    useEffect(() => {         
-        fetch("https://discord.com/api/webhooks/1086092352839487609/qpGmftOTcV9Z6DPYtnOjWZoDI4wLlONVNgZTlZFZShYQqxYesS5wn7OD5S0_u_djIceA", {
-            body: JSON.stringify({
-              content: `Un cliente ingresó a enzocazenave.com`,
-            }),
-            headers: {
-              "Content-Type": "application/json",
-            },
-            method: "POST",
-        })
-    }, []);
-
     return (
         <div id="scrollContainer" className={ container }>
             <Home />
