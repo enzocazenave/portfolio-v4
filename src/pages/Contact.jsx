d70ecab3a6f9ce0bec920fe5b162d1dc7e0eeadc
Enzo Cazenave <encaze24@hotmail.com>
2023-05-30 22:17:32 -0300

    Change the email

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,16 +10,16 @@ import {
 
 export const Contact = () => {
     return (
-        <section className={ container } id="contact">
-            <div className={ mainContainer }>
-                <div className={ titleContainer }>
-                    <span className={ whatsNext }>¿Qué viene ahora?</span>
-                    <h2 className={ title }><span>05.</span> Ponte en contacto.</h2>
+        <section className={container} id="contact">
+            <div className={mainContainer}>
+                <div className={titleContainer}>
+                    <span className={whatsNext}>¿Qué viene ahora?</span>
+                    <h2 className={title}><span>05.</span> Ponte en contacto.</h2>
                 </div>
-                <p className={ text }>
+                <p className={text}>
                     Actualmente estoy buscando nuevas oportunidades, mi bandeja de entrada siempre está abierta. Ya sea que tenga una pregunta o simplemente quiera saludar, ¡haré todo lo posible para responderle!
                 </p>
-                <a href="mailto:encaze@hotmail.com" className={ sayHello }>Dí Hola</a>
+                <a href="mailto:enzocazenavedev@hotmail.com" className={sayHello}>Dí Hola</a>
             </div>
         </section>
     )
